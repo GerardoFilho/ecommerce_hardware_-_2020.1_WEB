a3e1e5a3ead2ba8a5d608b18a3cdb7c758f67124
Ravi Leal <42724238+tronirix@users.noreply.github.com>
2020-10-14 17:56:58 -0300

    Initial commit

---
+++ src/config/database.ts
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+
+export default
+class Database {
+    url: string = 'mongodb://localhost/ecommerce'
+    options = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+
+    public constructor () {
+      mongoose.Promise = global.Promise
+      mongoose.connect(this.url, this.options)
+        .then(() => {
+          console.log('Connected to Database')
+        }).catch((err) => {
+          console.log('Not Connected to Database ERROR! ', err)
+        })
+    }
+}
