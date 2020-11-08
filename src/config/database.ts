66967d186d3db3eba25df2ac84e8beaf0ee2a71b
Ravi Leal <42724238+ravileal@users.noreply.github.com>
2020-11-08 02:46:55 -0300

    Teste variaveis de ambiente

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,20 +2,20 @@ import mongoose from 'mongoose'
 
 export default
 class Database {
-    //url: string = 'mongodb://localhost/ecommerce'
-    url: string = 'mongodb+srv://dev:devforweb@cluster0.ne9ih.mongodb.net/ecommerce?retryWrites=true&w=majority'
-    options = {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    }
+  // url: string = 'mongodb://localhost/ecommerce'
+  url: string = 'mongodb+srv://dev:devforweb@cluster0.ne9ih.mongodb.net/ecommerce?retryWrites=true&w=majority'
+  options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
 
-    public constructor () {
-      mongoose.Promise = global.Promise
-      mongoose.connect(this.url, this.options)
-        .then(() => {
-          console.log('Connected to Database')
-        }).catch((err) => {
-          console.log('Not Connected to Database ERROR! ', err)
-        })
-    }
+  public constructor () {
+    mongoose.Promise = global.Promise
+    mongoose.connect(process.env.MONGO_URL, this.options)
+      .then(() => {
+        console.log('Connected to Database')
+      }).catch((err) => {
+        console.log('Not Connected to Database ERROR! ', err)
+      })
+  }
 }
